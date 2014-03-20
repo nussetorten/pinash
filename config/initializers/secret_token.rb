8a503538c95e96167a53f79d906512640427206c
Nicholas Vitovitch <nicholas.vitovitch@sri.com>
2014-03-20 13:20:48 -0400

    Adds empty rails4 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinash::Application.config.secret_key_base = '0de8286e42bdc9952f10ca18c8145ee97db28df3bac9e9e041a5081647fb19f5e882e596fadc07747eb82fdd2570ca9287e6a1cd6b3154f899fdc658f6a445a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinash::Application.config.session_store :cookie_store, key: '_pinash_session'
